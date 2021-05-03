80dff69549a656c66e6d295f34cc9e08f6ba32f3
Andrew Nguyen <nguyena8047@gmail.com>
2021-05-03 11:49:57 -0700

    Pushing to switch to Ubuntu

--- backend/test.py
+++ backend/test.py
@@ -5,6 +5,7 @@ from sklearn.metrics import classification_report,confusion_matrix
 import tensorflow as tf
 import os
 import numpy as np
+from mtcnn.mtcnn import MTCNN
 
 import keras
 from keras.models import Sequential
@@ -12,25 +13,24 @@ from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 
-labels = ['baby', 'kid','teenager', 'adult', 'elderly']
+labels = ['baby', 'child','youth', 'middle_aged', 'senior']
 image_size = 300
 def get_data(data_dir):
     data = []
     for label in labels:
-        path  = os.path.join(data_dir, label)
+        path = os.path.join(data_dir, label)
         class_num = labels.index(label)
         for img in os.listdir(path):
             try:
-                img_arr = cv2.imread(os.path.join(path, img))[...,::,-1]
-                resized_arr = cv2.resize(img_arr, (img_size, img_size))
+                img_arr = cv.imread(os.path.join(path, img))[...,::,-1]
+                resized_arr = cv.resize(img_arr, (image_size, image_size))
                 data.append([resized_arr, class_num])
             except Exception as e:
                 print(e)
     return np.array(data)
 
-train = get_data('data/')
+# train = get_data('data/')
 l = []
-for i in train:
 
 
 
@@ -50,25 +50,48 @@ def image_text():
 
 classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-time_spent = 0
-cv.setUseOptimized(False)
-for i in range(10):
-    pixels = cv.imread(image_text(), 0)
-    interval = cv.getTickCount()
-    bboxes = classifier.detectMultiScale(pixels)
+# time_spent = 0
+# cv.setUseOptimized(False)
+# for i in range(10):
+#     pixels = cv.imread(image_text(), 0)
+#     interval = cv.getTickCount()
+#     bboxes = classifier.detectMultiScale(pixels)
 
-    for box in bboxes:
-        x, y, width, height = box
-        x2, y2 = x + width, y + height
-        cv.rectangle(pixels, (x, y), (x2, y2), (0, 0, 255), 1)
+#     for box in bboxes:
+#         x, y, width, height = box
+#         x2, y2 = x + width, y + height
+#         cv.rectangle(pixels, (x, y), (x2, y2), (0, 0, 255), 1)
 
-    cv.imshow('Face detection', pixels)
-    time_spent += (cv.getTickCount() - interval)/cv.getTickFrequency()
-    print('Total Time:', time_spent)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+#     cv.imshow('Face detection', pixels)
+#     time_spent += (cv.getTickCount() - interval)/cv.getTickFrequency()
+#     print('Total Time:', time_spent)
+#     cv.waitKey(0)
+#     cv.destroyAllWindows()
+
+def draw_faces(filename, result_list):
+    data = pyplot.imread(filename)
+    for i in range (len(result_list)):
+        x1, y1, width, height = result_list[i]['box']
+        x2, y2 = x1 + width, y1 + height
+        pyplot.subplot(1, len(result_list), i + 1)
+        pyplot.axis('off')
+        pyplot.imshow(data[y1:y2, x1:x2])
+    pyplot.show()
 
 
+
+
+def main():
+    print(tf.__version__)
+    # filename = image_text()
+    # pixels = pyplot.imread(filename)
+    # detector = MTCNN()
+    # faces = detector.detect_faces(pixels)
+    # draw_faces(filename, faces)
+    return
+
+if __name__ == '__main__':
+    main()
 
 
 
