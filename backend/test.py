854610c5a0ba5802ea1dcf668b4f2da7fc5d4c2d
AndrewNguyen5 <nguyena8047@gmail.com>
2021-05-08 02:52:19 -0700

    Added os scanning so it adds up all photos in baby

--- backend/test.py
+++ backend/test.py
@@ -7,27 +7,12 @@ import os
 import numpy as np
 from mtcnn.mtcnn import MTCNN
 
-labels = ['baby', 'child','youth', 'middle_aged', 'senior']
-image_size = 300
-def get_data(data_dir):
-    data = []
-    for label in labels:
-        path = os.path.join(data_dir, label)
-        class_num = labels.index(label)
-        for img in os.listdir(path):
-            try:
-                img_arr = cv.imread(os.path.join(path, img))[...,::,-1]
-                resized_arr = cv.resize(img_arr, (image_size, image_size))
-                data.append([resized_arr, class_num])
-            except Exception as e:
-                print(e)
-    return np.array(data)
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
 
-# train = get_data('data/')
-l = []
-
-
-image_number = 0
 
 # def image_text():
 #    global image_number
@@ -59,7 +44,7 @@ image_number = 0
 # cv.waitKey(0)
 # cv.destroyAllWindows()
 
-def draw_faces(filename, result_list):
+def draw_faces(filename, directory, name, result_list):
     data = pyplot.imread(filename)
     for i in range (len(result_list)):
         x1, y1, width, height = result_list[i]['box']
@@ -67,29 +52,64 @@ def draw_faces(filename, result_list):
         pyplot.subplot(1, len(result_list), i + 1)
         pyplot.axis('off')
         pyplot.imshow(data[y1:y2, x1:x2])
+        new_filename = 'cropped_photos/' + directory[5:] + '/' + name[0:-4] + '_' + str(i) + '.png'
+        pyplot.savefig(new_filename, bbox_inches='tight')
+        pyplot.clf()
+        pyplot.cla()
+
     # pyplot.show()
-    new_filename = 'cropped_photos/baby/11601.png'
-    pyplot.savefig(new_filename)
-    pyplot.show()
     return new_filename
 
-# def draw_edges(filename, result_list):
-#     data = pyplot.imread(filename)
-#     for i in range (len(result_list)):
-#         points = result_list[i]['keypoints'].values()
-#         print(points)
-#         pyplot.subplot(1, len(result_list), i + 1)
-#         pyplot.axis('off')
-#     return
+def get_data(data_dir):
+    data = []
+    image_size = 300
+    for label in labels:
+        path = os.path.join(data_dir, label)
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv.imread(os.path.join(path, img))[...,::,-1]
+                resized_arr = cv.resize(img_arr, (image_size, image_size))
+                data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data)
+
+
+def define_age_of_faces():
+    labels = ['baby', 'child', 'youth', 'middle_aged', 'senior']
+
+    train = get_data('data/')
+    validation = get_data('data/')
+    l = []
+
+
+    image_number = 0
+
 
 def main():
-    filename = 'data/baby/11601.jpg'
-    pixels = pyplot.imread(filename)
+    current_path = os.path.dirname(__file__)
+    data_path = os.path.join(current_path, 'data')
+    baby_path = os.path.join(data_path, 'baby')
+    baby_photos = []
+    with os.scandir(baby_path) as it:
+        for entry in it:
+            if (entry.name.endswith('.png') or entry.name.endswith('.jpg')) and entry.is_file():
+                baby_photos.append((entry.name, entry.path))
+    # print(baby_photos)
     detector = MTCNN()
-    faces = detector.detect_faces(pixels)
-    print(faces)
+    for photo in baby_photos:
+        pixels = pyplot.imread(photo[1])
+        faces = detector.detect_faces(pixels)
+        # print(faces)
+        print(draw_faces(photo[1], baby_path, photo[0], faces))
 
-    new_filename = draw_faces(filename, faces)
+    # pixels = pyplot.imread(filename)
+    # detector = MTCNN()
+    # faces = detector.detect_faces(pixels)
+    # print(faces)
+    #
+    # new_filename = draw_faces(filename, faces)
     # print(new_filename)
     # pixels2 = pyplot.imread(new_filename)
     # print(pixels2)
