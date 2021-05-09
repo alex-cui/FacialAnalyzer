939d9878a8c5158598d90ee9caa61aaa7c6bc7b3
AndrewNguyen5 <nguyena8047@gmail.com>
2021-05-09 03:28:31 -0700

    Added some error handling for cropping and removed problematic photos

--- backend/test.py
+++ backend/test.py
@@ -13,54 +13,39 @@ from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 
-
-# def image_text():
-#    global image_number
-#    text = 'data/images/'
-#    num = str(image_number)
-#    for i in range(5 - len(num)):
-#        text += '0'
-#    text += num
-#    text += '.png'
-#    image_number += 1
-#    return text
-
-# classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-#
-# time_spent = 0
-# cv.setUseOptimized(False)
-# pixels = cv.imread('data/baby/11601.jpg')
-# interval = cv.getTickCount()
-# bboxes = classifier.detectMultiScale(pixels)
-#
-# for box in bboxes:
-#     x, y, width, height = box
-#     x2, y2 = x + width, y + height
-#     cv.rectangle(pixels, (x, y), (x2, y2), (0, 0, 255), 1)
-#
-# cv.imshow('Face detection', pixels)
-# time_spent += (cv.getTickCount() - interval)/cv.getTickFrequency()
-# print('Total Time:', time_spent)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
-
 def draw_faces(filename, directory, name, result_list):
     data = pyplot.imread(filename)
+    new_filename = ''
     for i in range (len(result_list)):
         x1, y1, width, height = result_list[i]['box']
         x2, y2 = x1 + width, y1 + height
         pyplot.subplot(1, len(result_list), i + 1)
         pyplot.axis('off')
+        if data[y1:y2, x1:x2].shape[0] <= 0 or data[y1:y2, x1:x2].shape[1] <= 0:
+            continue
+        # print(data[y1:y2, x1:x2].shape)
         pyplot.imshow(data[y1:y2, x1:x2])
         new_filename = 'cropped_photos/' + directory[5:] + '/' + name[0:-4] + '_' + str(i) + '.png'
         pyplot.savefig(new_filename, bbox_inches='tight')
         pyplot.clf()
         pyplot.cla()
-
-    # pyplot.show()
     return new_filename
 
-def get_data(data_dir):
+def crop_photos(path):
+    photos = []
+    with os.scandir(path) as it:
+        for entry in it:
+            if (entry.name.endswith('.png') or entry.name.endswith('.jpg')) and entry.is_file():
+                photos.append((entry.name, entry.path))
+    detector = MTCNN()
+    for photo in photos:
+        print(photo[0])
+        pixels = pyplot.imread(photo[1])
+        faces = detector.detect_faces(pixels)
+        draw_faces(photo[1], path, photo[0], faces)
+        # print(draw_faces(photo[1], path, photo[0], faces))
+
+def get_data(data_dir, labels):
     data = []
     image_size = 300
     for label in labels:
@@ -79,25 +64,13 @@ def get_data(data_dir):
 def define_age_of_faces():
     labels = ['baby', 'child', 'youth', 'middle_aged', 'senior']
 
-    train = get_data('data/')
-    validation = get_data('data/')
-    l = []
+    train = get_data('cropped_photos/', labels)
+    validation = get_data('cropped_photos/', labels)
+    # print(train)
+    # print(validation)
 
 
     image_number = 0
-
-def crop_photos(path):
-    photos = []
-    with os.scandir(path) as it:
-        for entry in it:
-            if (entry.name.endswith('.png') or entry.name.endswith('.jpg')) and entry.is_file():
-                photos.append((entry.name, entry.path))
-    detector = MTCNN()
-    for photo in photos:
-        pixels = pyplot.imread(photo[1])
-        faces = detector.detect_faces(pixels)
-        # print(faces)
-        print(draw_faces(photo[1], path, photo[0], faces))
 
 
 def main():
@@ -105,18 +78,14 @@ def main():
     # make sure you have a directory like cropped_photos/baby and cropped_photos/child ... etc
     # if you want you can add a new folder into data to receive the images
     # but make sure you add a new folder into cropped_folders with the same name
-    crop_photos('data/child')
+    # crop_photos('data/baby')
+    # crop_photos('data/child')
+    # crop_photos('data/youth')
+    # crop_photos('data/middle_aged')
+    crop_photos('data/senior')
 
-    # pixels = pyplot.imread(filename)
-    # detector = MTCNN()
-    # faces = detector.detect_faces(pixels)
-    # print(faces)
-    #
-    # new_filename = draw_faces(filename, faces)
-    # print(new_filename)
-    # pixels2 = pyplot.imread(new_filename)
-    # print(pixels2)
-    # detector2 = MTCNN()
+    define_age_of_faces()
+
     return
 
 if __name__ == '__main__':
