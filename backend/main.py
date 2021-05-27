7337d7b7954563eab2ed08ff6ff46502fa533797
alex-cui <alexcui1@yahoo.com>
2021-05-27 01:48:48 -0700

    cleaned up final draft for frontend

--- backend/main.py
+++ backend/main.py
@@ -14,11 +14,11 @@ CORS(app, support_credentials=True)
 def index(): 
 	return "Flask server" 
  
+#test request
 @app.route('/postdata', methods = ['POST']) 
 def postdata(): 
     data = request.get_json() 
     print(data) 
-    # do something with this data variable that contains the data from the node server 
     return json.dumps({"newdata":"hereisthenewdatayouwanttosend"}) 
 
 @app.route('/detect', methods = ['POST']) 
@@ -38,12 +38,8 @@ def guessAge():
     data = json.dumps(data) #converts a Python object into a json string
     data = json.loads(data)
 
-    # print(data['imgs'][0][-20])
-    # print(data['imgs'][1][-20])
-    # print(data['imgs'][2][-20])
-
-    allAges = [train.guess('age.json', b64) for b64 in data['imgs']]
-    return json.dumps({"data" : allAges}) 
+    ret = [train.guess('age.json', b64) for b64 in data['imgs']]
+    return json.dumps({"data" : ret}) 
 
 @app.route('/guessGender', methods = ['POST']) 
 def guessGender(): 
@@ -51,8 +47,17 @@ def guessGender():
     data = json.dumps(data) #converts a Python object into a json string
     data = json.loads(data)
 
-    allAges = [train.guess('gender.json', b64) for b64 in data['imgs']]
-    return json.dumps({"data" : allAges}) 
+    ret = [train.guess('gender.json', b64) for b64 in data['imgs']]
+    return json.dumps({"data" : ret}) 
+
+@app.route('/guessCelebrity', methods = ['POST']) 
+def guessCelebrity(): 
+    data = request.get_json()
+    data = json.dumps(data) #converts a Python object into a json string
+    data = json.loads(data)
+
+    ret = [train.guess('new_celeb.json', b64) for b64 in data['imgs']]
+    return json.dumps({"data" : ret}) 
 
 if __name__ == "__main__": 
 	app.run(host='0.0.0.0', port=5000, debug=True) 
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,149 +11,159 @@ export class AppComponent {
   public loading : boolean = false;
   public faces = [];
   public backendURL = "http://127.0.0.1:5000";
-  // public backendURL2 = "http://127.0.0.1:5000";
-  // public backendURL = "http://node0.alexcui-qv98630.cs179icloud-pg0.utah.cloudlab.us:5000";
+  // public backendURL = "http://node0.alexcui-qv98630.cs179icloud-pg0.utah.cloudlab.us:30604";
 
   public ages = {};
   public genders = {};
+  public celebs = {};
+
   public guessAge = true;
   public guessGender = true;
+  public guessCeleb = true;
 
-  public faceTime = "";
+  public faceTime = 0.00;
+  public guessTime = 0.00;
   public ageTime = "";
   public genderTime = "";
-
+  public celebTime = "";
 
 
   constructor(private http: HttpClient) {}
   
   public uploadImage() {
-    this.http.post(this.backendURL + "/postdata", {img:0}).subscribe(res2 => {
-      console.log(res2);
-    }, err => {
-      console.log(err);
-    });
-      document.getElementById("hidden-button").click();
+    document.getElementById("hidden-button").click();
   }
 
   public loadFile(event) {
     var output = <HTMLImageElement>document.getElementById("img");
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-      URL.revokeObjectURL(output.src) // free memory
+    if (event.target.files[0]) {
+      output.src = URL.createObjectURL(event.target.files[0]);
+      output.onload = function() {
+        URL.revokeObjectURL(output.src) // free memory
+      }
     }
   }
 
   public detect() {
-    var image = document.getElementById("img");
     var inputImage = <HTMLInputElement>document.getElementById("hidden-button");
+    if (!inputImage.files[0]) {
+      return;
+    }
+    var image = document.getElementById("img");
     image.style.opacity = ".3";
 
     this.getBase64(inputImage.files[0]).then(
-        b64 => {
-            // console.log(b64);
-            this.loading = true;
-            this.resetData();
-            let time = performance.now();
+      b64 => {
+          this.loading = true;
+          this.resetData();
+          let time = performance.now();
 
-            //find all faces
-            this.http.post(this.backendURL + "/detect", {img: b64}).subscribe(res => {
-              console.log("done");
-              this.faceTime = this.getTimer(time);
+          //find all faces
+          this.http.post(this.backendURL + "/detect", {img: b64}).subscribe(res => {
 
-              let tempFaces = [];
-              for (let r of <any>res['data']) {
-                tempFaces.push("data:image/png;base64," + r);
-              }
+            let tempFaces = [];
+            for (let r of <any>res['data']) {
+              tempFaces.push("data:image/png;base64," + r);
+            }
 
-              for (let i = 0; i < res['data'].length; i++) {
-                this.faces.push({
-                  src: tempFaces[i],
-                });
-              }
+            for (let i = 0; i < res['data'].length; i++) {
+              this.faces.push({
+                src: tempFaces[i],
+              });
+            }
 
-              let ageDone = false;
-              let genderDone = false;
+            this.faceTime = parseFloat(this.getTimer(time));
+            let flag = 0;
+            let greatestTime = 0.00;
 
-              //guess age of faces
-              if (this.guessAge) {
-                let t1 = performance.now();
+            //guess age of faces
+            if (this.guessAge) {
+              flag++;
+              let t1 = performance.now();
 
-                this.http.post(this.backendURL + "/guessAge", {imgs: tempFaces}).subscribe(res2 => {
-                  console.log(res2);
-                  this.ageTime = this.getTimer(t1);
+              this.http.post(this.backendURL + "/guessAge", {imgs: tempFaces}).subscribe(res2 => {          
+                //add data to faces
+                for (let i = 0; i < res['data'].length; i++) {
+                  let age = res2['data'][i].class;
+          
+                  this.faces[i]['age'] = age;
+                  this.faces[i]['ageConfidence'] = res2['data'][i].confidence;
+                  this.ages[age] = this.ages[age] ? (this.ages[age] + 1) : 1;
+                }
 
-                  for (let i = 0; i < res['data'].length; i++) {
-                    let age = res2['data'][i].class;
-  
-                    this.faces[i]['age'] = age;
-                    this.faces[i]['ageConfidence'] = res2['data'][i].confidence;
-                    this.ages[age] = this.ages[age] ? (this.ages[age] + 1) : 1;
-                  }
+                this.ageTime = this.getTimer(t1);
+                if (parseFloat(this.ageTime) > greatestTime) {
+                  greatestTime = parseFloat(this.ageTime);
+                }
+                this.checkFinishedGuessing(--flag, greatestTime, image)
+              }, err => {
+                console.log(err);
+              });
+            }
 
-                  if (this.guessGender) {
-                    if (genderDone) {
-                      image.style.opacity = "1";
-                      this.loading = false;
-                    }
-                  }
-                  else {
-                    image.style.opacity = "1";
-                    this.loading = false;
-                  }
-                  ageDone = true;
-                }, err => {
-                  console.log(err);
-                  image.style.opacity = "1";
-                  this.loading = false;
-                });
-              }
+            //guess gender of faces
+            if (this.guessGender) {
+              flag++;
+              let t1 = performance.now();
 
-              //guess gender of faces
-              if (this.guessGender) {
-                let t1 = performance.now();
+              this.http.post(this.backendURL + "/guessGender", {imgs: tempFaces}).subscribe(res2 => {
+                //add data to faces
+                for (let i = 0; i < res2['data'].length; i++) {
+                  let gender = res2['data'][i].class;
 
-                this.http.post(this.backendURL + "/guessGender", {imgs: tempFaces}).subscribe(res3 => {
-                  console.log(res3);
-                  this.genderTime = this.getTimer(t1);
+                  this.faces[i]['gender'] = gender;
+                  this.faces[i]['genderConfidence'] = res2['data'][i].confidence;
+                  this.genders[gender] = this.genders[gender] ? (this.genders[gender] + 1) : 1;
+                }
 
-                  for (let i = 0; i < res3['data'].length; i++) {
-                    let gender = res3['data'][i].class;
-  
-                    this.faces[i]['gender'] = gender;
-                    this.faces[i]['genderConfidence'] = res3['data'][i].confidence;
-                    this.genders[gender] = this.genders[gender] ? (this.genders[gender] + 1) : 1;
-                  }
+                this.genderTime = this.getTimer(t1);
+                if (parseFloat(this.genderTime) > greatestTime) {
+                  greatestTime = parseFloat(this.genderTime);
+                }
+                this.checkFinishedGuessing(--flag, greatestTime, image)
+              }, err => {
+                console.log(err);
+              });
+            }
 
-                  if (this.guessAge) {
-                    if (ageDone) {
-                      image.style.opacity = "1";
-                      this.loading = false;
-                    }
-                  }
-                  else {
-                    image.style.opacity = "1";
-                    this.loading = false;
-                  }
-                  genderDone = true;
-                }, err => {
-                  console.log(err);
-                  image.style.opacity = "1";
-                  this.loading = false;
-                });
-              }
+            //guess closest looking celebrity
+            if (this.guessCeleb) {
+              flag++;
+              let t1 = performance.now();
 
-              //just done
-              if (!this.guessAge && !this.guessGender) {
-                image.style.opacity = "1";
-                this.loading = false;
-              }
-            }, err => {
-              console.log(err);
+              this.http.post(this.backendURL + "/guessCelebrity", {imgs: tempFaces}).subscribe(res2 => {
+                //add data to faces
+                for (let i = 0; i < res2['data'].length; i++) {
+                  let celeb = res2['data'][i].class;
+
+                  this.faces[i]['celeb'] = celeb;
+                  this.faces[i]['celebConfidence'] = res2['data'][i].confidence;
+                  this.celebs[celeb] = this.celebs[celeb] ? (this.celebs[celeb] + 1) : 1;
+                }
+
+                this.celebTime = this.getTimer(t1);
+                if (parseFloat(this.celebTime) > greatestTime) {
+                  greatestTime = parseFloat(this.celebTime);
+                }
+                this.checkFinishedGuessing(--flag, greatestTime, image)
+              }, err => {
+                console.log(err);
+              });
+            }
+
+            //just done
+            if (!this.guessAge && !this.guessGender && !this.guessCeleb) {
               image.style.opacity = "1";
               this.loading = false;
-            });
-        } 
+            }
+          }, err => {
+            console.log(err);
+            image.style.opacity = "1";
+            this.loading = false;
+          });
+      }, err => {
+        console.log(err)
+      }
     );
   }
 
@@ -161,13 +171,36 @@ export class AppComponent {
     this.faces = [];
     this.ages = {};
     this.genders = {};
-    this.faceTime = "";
+    this.celebs = {};
+    this.faceTime = 0.00;
+    this.guessTime = 0.00;
     this.ageTime = "";
     this.genderTime = "";
   }
 
+  public checkFinishedGuessing(flag, greatestTime, image) {
+    if (flag == 0) {
+      this.guessTime = greatestTime;
+      image.style.opacity = "1";
+      this.loading = false;
+    }
+  }
+
   public getTimer(t) {
     return ((performance.now() - t) / 1000).toFixed(2);
+  }
+
+  public getTimeInfo() {
+    let s = "";
+
+    if (this.guessAge && this.ageTime)
+      s += "Guessed ages in: " + this.ageTime + " second(s)\n";
+    if (this.guessGender && this.genderTime)
+      s += "Guessed genders in: " + this.genderTime + " second(s)\n";
+    if (this.guessCeleb && this.celebTime)
+      s += "Guessed celebrities in: " + this.celebTime + " second(s)";
+
+    return s;
   }
 
   public getBase64(file) {
